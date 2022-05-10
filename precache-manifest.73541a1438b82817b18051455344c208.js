121c8115ecf410c7f53fd22cd26b356350c048de
SHRINIVASAPRASADA1 <prasadashrinivasa@gmail.com>
2022-05-10 13:59:52 +0530

    Updates

---
+++ precache-manifest.73541a1438b82817b18051455344c208.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff4d9adc0b14a795028e",
+    "url": "/curdOnApi/static/css/main.b0b8689a.chunk.css"
+  },
+  {
+    "revision": "ff4d9adc0b14a795028e",
+    "url": "/curdOnApi/static/js/main.ff4d9adc.chunk.js"
+  },
+  {
+    "revision": "4f473d823a9b02f8d234",
+    "url": "/curdOnApi/static/js/1.4f473d82.chunk.js"
+  },
+  {
+    "revision": "be925d40a55e153d2059",
+    "url": "/curdOnApi/static/js/2.be925d40.chunk.js"
+  },
+  {
+    "revision": "ccc3ddc9289b6c002e2f",
+    "url": "/curdOnApi/static/js/runtime~main.ccc3ddc9.js"
+  },
+  {
+    "revision": "c7b7d495f6b8980b615b1a8afb2e8239",
+    "url": "/curdOnApi/index.html"
+  }
+];
